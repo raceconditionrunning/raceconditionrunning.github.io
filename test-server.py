ed4887b72fbc48566ddd42b75a5a5ebf66b4d5fe
Zach Tatlock <ztatlock@cs.washington.edu>
2016-09-20 21:13:40 -0700

    add route db

---
+++ test-server.py
@@ -0,0 +1,8 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
