d27b9fb137cdadd65823121473ceb2aa2d14b685
Nick Walker <nick@nickwalker.us>
2024-03-24 22:50:23 -0700

    Factor out masonry image gallery
    Will part out components that have been used on multiple
    event pages as need arises and time allows
    Move table helpers into separate file so common has no deps

---
+++ js/MasonryImageGallery.js
@@ -0,0 +1,92 @@
+import "masonry-layout";
+import {prepareImagesForPhotoswipe} from "./common.js";
+import PhotoSwipeLightbox from "photoswipe-lightbox";
+
+export class MasonryImageGallery extends HTMLElement {
+    constructor() {
+        super(); // Always call super first in the constructor
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        // Here you'll add the HTML structure and initialize masonry and lightbox
+        const imagePath = this.getAttribute('base-url');
+        const fileExtension = this.getAttribute('file-extension');
+        const imageNames = (this.getAttribute('image-names')).split('|');
+
+        const galleryHTML = "<div class='grid-sizer'></div>" + imageNames.map(name => `
+      <a href="${imagePath}${name}.webp">
+        <img loading="lazy" src="${imagePath}${name}${fileExtension}"/>
+      </a>
+    `).join('');
+
+
+        const styles = `<style>
+    /* GALLERY **************************/
+    .masonry {
+        background-color: rgba(0,0,0,.05);
+    }
+    
+    /* clear fix */
+    .masonry:after {
+        content: '';
+        display: block;
+        clear: both;
+    }
+    .masonry > a {
+        float: left;
+    }
+    
+    .masonry img {
+        display: block;
+        max-width: 100%;
+    }
+    
+    .masonry .grid-sizer,
+    .masonry a {
+        width: 50%;
+    }
+    
+    @media screen and (width > 720px) {
+    .masonry .grid-sizer,
+    .masonry a {
+        width: 33.33%;
+    }
+    }
+    
+    .pswp img {
+    max-width: none;
+    object-fit: contain;
+    }
+    </style>`;
+
+        this.shadowRoot.innerHTML = `${styles}<div class="masonry">${galleryHTML}</div>`;
+        prepareImagesForPhotoswipe(this.shadowRoot.querySelectorAll("a")).then(() => {
+            const lightbox = new PhotoSwipeLightbox({
+                gallery: this.shadowRoot.querySelector(".masonry"),
+                children: 'a',
+                showHideAnimationType: 'zoom',
+                showAnimationDuration: 200,
+                hideAnimationDuration: 200,
+                pswpModule: () => import("photoswipe")
+
+            });
+            lightbox.init();
+
+            let container = this.shadowRoot.querySelector(".masonry");
+            let msnry = new Masonry(container, {
+                itemSelector: 'a',
+                columnWidth: '.grid-sizer',
+                percentPosition: true
+            });
+            msnry.layout()
+        });
+    }
+
+}
+
+customElements.define('masonry-image-gallery', MasonryImageGallery);
--- js/common.js
+++ js/common.js
@@ -1,6 +1,4 @@
-import {Tabulator, ColumnCalcsModule, GroupRowsModule, FormatModule, InteractionModule, ResizeColumnsModule, ResizeTableModule, ResponsiveLayoutModule, SortModule, SelectRowModule} from 'tabulator-tables';
 
-Tabulator.registerModule([ColumnCalcsModule, FormatModule, GroupRowsModule, InteractionModule, ResizeColumnsModule, ResizeTableModule, ResponsiveLayoutModule, SortModule, SelectRowModule]);
 export function formatPace(seconds, unit = " /mi") {
     let date = new Date(0);
     date.setSeconds(seconds);
@@ -244,82 +242,6 @@ export function processRelayGeoJSON(relay) {
     return [legs, exchanges]
 }
 
-export function createScheduleTable(container, schedule, startTime) {
-    let firstLeg = {}
-    let legs = new Array(schedule.length)
-    for (let leg of schedule) {
-        for (let runner of leg.runners) {
-            firstLeg[runner] = Math.min(firstLeg[runner] ?? Number.MAX_VALUE, leg.leg)
-        }
-        legs[leg.leg] = leg
-    }
-    legs[0].start_time = new Date(startTime.getTime())
-    for (let i = 1; i < legs.length; i++) {
-        legs[i].start_time = new Date((legs[i - 1].start_time.getTime() + legs[i].pace_mi * legs[i].distance_mi * 1000 + 4 * 60 * 1000))
-    }
-    let finishTimeCalc = function(values, data, calcParams){
-        if (data.length === 0) return;
-        let lastLeg = data[data.length - 1]
-        let endDate = new Date((lastLeg.start_time.getTime() + lastLeg.pace_mi * lastLeg.distance_mi * 1000 + 4 * 60 * 1000));
-        return endDate.toTimeString().split(" ")[0].substring(0, 5)
-    }
-
-    let scheduleTable = new Tabulator(container, {
-        data: schedule,
-        layout: "fitDataFill",
-        responsiveLayout: "collapse",
-        selectable: false,
-        columns: [
-            {title: "Leg", field: "leg", headerSort:false, resizable: false, bottomCalc: ()=> "Finish"},
-            {title: "Time", field: "start_time", formatter: cell => cell.getValue().toTimeString().split(" ")[0].substring(0, 5), bottomCalc: finishTimeCalc, headerSort:false, resizable: false},
-            {title: "Start", field: "start_exchange", headerSort:false, resizable: false},
-            {title: "Miles", field: "distance_mi", bottomCalc: "sum", bottomCalcParams: {precision: 2}, headerSort:false, resizable: false},
-            {
-                title: "Pace (/mi)", field: "pace_mi",  formatter: cell => formatPace(cell.getValue(), ""), headerSort:false, resizable: false
-            },
-            {
-                title: "Elev. Gain (ft)", field: "ascent_ft", bottomCalc: "sum", headerSort:false, resizable: false
-            },
-            {title: "Runners", field: "runners", formatter: cell => {
-                    let row = cell.getRow().getData()
-                    let out = []
-                    for (let runner of cell.getValue()) {
-                        let classes = ""
-                        if (runner === row.leader) {
-                            classes += "leg-leader "
-                        }
-                        if (firstLeg[runner] === row.leg) {
-                            classes += "first-leg "
-                        }
-                        out.push(`<span class="schedule-runner ${classes}">${runner}</span>`)
-                    }
-                    return out.join(", ")
-                }, headerSort:false, resizable: false},
-        ]
-    })
-    // Bottom calcs will truncate unless we redraw manually
-    scheduleTable.on("dataProcessed", () => scheduleTable.redraw())
-}
-
-export function createLegDetailsTable(container, legsGeojson, exchangesGeoJson) {
-    let data = []
-    let legDescriptions = ""
-
-    function formatLegDescription(startExchange, endExchange, leg){
-        let legNumber = `<span class="leg-number">${leg.id}:</span> `
-        let legName = `${legNumber}<a href="${startExchange.stationInfo}" target="_new">${startExchange.name}</a> to <a href="${endExchange.stationInfo}" target="_new">${endExchange.name}</a>`
-
-        return `<div class="d-flex flex-column flex-lg-row justify-content-between align-items-baseline"><h5>${legName}</h5><h6>${leg.distance_mi.toFixed(2)}mi ↑${leg.ascent_ft.toFixed(0)}ft ↓${leg.descent_ft.toFixed(0)}ft</h6></div><i>Landmark: ${startExchange.landmark}</i><p class="mb-0">${leg.notes}</p>`
-    }
-    for (let leg of legsGeojson) {
-        let legData = leg.properties
-        legData.id = legData.start_exchange + 1
-        data.push(legData)
-        legDescriptions += `<div class="mb-4">${formatLegDescription(exchangesGeoJson[legData.start_exchange].properties, exchangesGeoJson[legData.end_exchange].properties, legData)}</div>`
-    }
-    container.innerHTML = legDescriptions
-
-}
 
 export async function prepareImagesForPhotoswipe(galleryElements) {
     // PhotoSwipe (lightbox) expects the width and height of each image to be set in the DOM. This function
@@ -335,11 +257,17 @@ export async function prepareImagesForPhotoswipe(galleryElements) {
             thumbImage.onload = () => {
                 // This promise only completes when lazy load is triggered by user interaction (or no lazy attribute
                 // is used.
-                element.dataset.pswpWidth = thumbImage.width;
-                element.dataset.pswpHeight = thumbImage.height;
+                element.dataset.pswpWidth = thumbImage.naturalWidth;
+                element.dataset.pswpHeight = thumbImage.naturalHeight;
                 resolve(); // Resolve the promise only if the image has been loaded
             }
             thumbImage.onerror = () => { resolve(); };
+            // onload may not trigger if the image is already in cache, so we need to check if it's already loaded
+            if (thumbImage.complete && thumbImage.naturalWidth !== 0) {
+                element.dataset.pswpWidth = thumbImage.naturalWidth;
+                element.dataset.pswpHeight = thumbImage.naturalHeight;
+                resolve();
+            }
         });
         promisesList.push(promise);
     });
