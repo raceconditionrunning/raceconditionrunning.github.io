5c2342555a992ae09519591f6b33dd90eb64fa2e
Nick Walker <nick@nickwalker.us>
2024-11-12 01:27:28 -0800

    Rename scripts
    Fix unzip usage
    Fail on errors in bash, and print each command issued
    Run transit update last

--- _bin/update_routes_and_locs.sh
+++ _bin/update_routes_and_locs.sh
@@ -1,3 +1,4 @@
+set -xe
 # King County Metro: https://www.soundtransit.org/GTFS-KCM/google_transit.zip
 # Pierce: https://www.soundtransit.org/GTFS-PT/gtfs.zip
 # Intercity: https://gtfs.sound.obaweb.org/prod/19_gtfs.zip
@@ -10,6 +11,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+python $SCRIPT_DIR/update_route_neighborhoods.py
+
 LIGHT_RAIL_PATH=https://www.soundtransit.org/GTFS-rail/40_gtfs.zip
 BUS_PATH=https://www.soundtransit.org/GTFS-KCM/google_transit.zip
 FERRY_PATH=https://gtfs.sound.obaweb.org/prod/95_gtfs.zip
@@ -17,13 +20,12 @@ FERRY_PATH=https://gtfs.sound.obaweb.org/prod/95_gtfs.zip
 mkdir -p $SCRIPT_DIR/tmp/
 
 wget -c $BUS_PATH -O $SCRIPT_DIR/tmp/bus.zip
-unzip $SCRIPT_DIR/tmp/bus.zip $SCRIPT_DIR/tmp/bus
-mv $SCRIPT_DIR/tmp/bus/stops.txt $SCRIPT_DIR/seattle_transit_data/bus.csv
+unzip -o $SCRIPT_DIR/tmp/bus.zip -d $SCRIPT_DIR/tmp/bus
+mv $SCRIPT_DIR/tmp/bus/stops.txt $SCRIPT_DIR/../routes/seattle_transit_data/bus.csv
 
 wget -c $LIGHT_RAIL_PATH -O $SCRIPT_DIR/tmp/light_rail.zip
-unzip $SCRIPT_DIR/tmp/light_rail.zip $SCRIPT_DIR/tmp/light_rail
-mv $SCRIPT_DIR/tmp/light_rail/stops.txt $SCRIPT_DIR/seattle_transit_data/light_rail.csv
+unzip -o $SCRIPT_DIR/tmp/light_rail.zip -d $SCRIPT_DIR/tmp/light_rail
+mv $SCRIPT_DIR/tmp/light_rail/stops.txt $SCRIPT_DIR/../routes/seattle_transit_data/light_rail.csv
 
 rm -rf $SCRIPT_DIR/tmp/
-python $SCRIPT_DIR/extract_locs_info.py
-python $SCRIPT_DIR/extract_routes_info.py
+python $SCRIPT_DIR/update_location_transit.py
