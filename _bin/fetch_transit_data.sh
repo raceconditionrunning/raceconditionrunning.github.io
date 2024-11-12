9ea92a91b36cb30162c25d03eef65c81ab5facdc
Nick Walker <nick@nickwalker.us>
2024-11-12 01:43:15 -0800

    Remove unused transit stop loading code
    We won't track transit data in repo until we have a strategy for running
    this in CI, or for serving the stop locations on a map

--- _bin/fetch_transit_data.sh
+++ _bin/fetch_transit_data.sh
@@ -11,21 +11,25 @@ set -xe
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python $SCRIPT_DIR/update_route_neighborhoods.py
-
 LIGHT_RAIL_PATH=https://www.soundtransit.org/GTFS-rail/40_gtfs.zip
 BUS_PATH=https://www.soundtransit.org/GTFS-KCM/google_transit.zip
 FERRY_PATH=https://gtfs.sound.obaweb.org/prod/95_gtfs.zip
+NEIGHBORHOOD_PATH="https://hub.arcgis.com/api/v3/datasets/b4a142f592e94d39a3bf787f3c112c1d_0/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1"
 
-mkdir -p $SCRIPT_DIR/tmp/
+
+mkdir -p $SCRIPT_DIR/tmp/ $SCRIPT_DIR/../routes/transit_data/
+wget -c $NEIGHBORHOOD_PATH -O $SCRIPT_DIR/../routes/neighborhoods.geojson
+
+wget -c $FERRY_PATH -O $SCRIPT_DIR/tmp/ferry.zip
+unzip -o $SCRIPT_DIR/tmp/ferry.zip -d $SCRIPT_DIR/tmp/ferry
+mv $SCRIPT_DIR/tmp/ferry/stops.txt $SCRIPT_DIR/../routes/transit_data/ferry.csv
 
 wget -c $BUS_PATH -O $SCRIPT_DIR/tmp/bus.zip
 unzip -o $SCRIPT_DIR/tmp/bus.zip -d $SCRIPT_DIR/tmp/bus
-mv $SCRIPT_DIR/tmp/bus/stops.txt $SCRIPT_DIR/../routes/seattle_transit_data/bus.csv
+mv $SCRIPT_DIR/tmp/bus/stops.txt $SCRIPT_DIR/../routes/transit_data/bus.csv
 
 wget -c $LIGHT_RAIL_PATH -O $SCRIPT_DIR/tmp/light_rail.zip
 unzip -o $SCRIPT_DIR/tmp/light_rail.zip -d $SCRIPT_DIR/tmp/light_rail
-mv $SCRIPT_DIR/tmp/light_rail/stops.txt $SCRIPT_DIR/../routes/seattle_transit_data/light_rail.csv
+mv $SCRIPT_DIR/tmp/light_rail/stops.txt $SCRIPT_DIR/../routes/transit_data/light_rail.csv
 
 rm -rf $SCRIPT_DIR/tmp/
-python $SCRIPT_DIR/update_location_transit.py
